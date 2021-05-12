03abec731dc3c7e2df1808331637dafb6ee457e9
nacro90 <orcan.tiryakioglu@gmail.com>
2021-05-12 21:22:52 +0300

    Refactor the code

--- lua/numb/init.lua
+++ lua/numb/init.lua
@@ -1,18 +1,29 @@
 local numb = {}
 
 local api = vim.api
+local cmd = vim.cmd
+
+-- Make nvim_exec more clutter free
+local function nvim_exec(c) return api.nvim_exec(c, true) end
 
 local log = require('numb.log')
 
 -- Stores windows original states
 local win_states = {}
-local opts = {show_numbers = true, show_cursorline = true}
+
+-- Options with default values
+local opts = {
+   show_numbers = true, -- Enable 'number' for the window while peeking
+   show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+}
 
 local function peek(winnr, linenr)
    local bufnr = api.nvim_win_get_buf(winnr)
    local n_buf_lines = api.nvim_buf_line_count(bufnr)
    linenr = math.min(linenr, n_buf_lines)
    linenr = math.max(linenr, 1)
+
+   -- Saving window state if this is a first call of peek()
    if not win_states[winnr] then
       win_states[winnr] = {
          cursor = api.nvim_win_get_cursor(winnr),
@@ -24,12 +35,14 @@ local function peek(winnr, linenr)
       }
    end
 
+   -- Set window options for peeking
    if opts.show_numbers then api.nvim_win_set_option(winnr, 'number', true) end
    if opts.show_cursorline then
       api.nvim_win_set_option(winnr, 'cursorline', true)
    end
    api.nvim_win_set_option(winnr, 'foldenable', false)
 
+   -- Setting the cursor
    local original_column = win_states[winnr].cursor[2]
    local peek_cursor = {linenr, original_column}
    api.nvim_win_set_cursor(winnr, peek_cursor)
@@ -40,18 +53,18 @@ local function unpeek(winnr, stay)
 
    if not orig_state then return end
 
+   -- Restoring necessary window options
    if opts.show_numbers then
       api.nvim_win_set_option(winnr, 'number', orig_state.options.number)
    end
    if opts.show_cursorline then
       api.nvim_win_set_option(winnr, 'cursorline', orig_state.options.cursorline)
    end
-
    api.nvim_win_set_option(winnr, 'foldenable', orig_state.options.foldenable)
 
    if stay then
       -- Unfold at the cursorline if user wants to stay
-      vim.cmd('normal! zv')
+      cmd('normal! zv')
    else
       -- Rollback the cursor if the user does not want to stay
       api.nvim_win_set_cursor(winnr, orig_state.cursor)
@@ -65,10 +78,10 @@ function numb.on_cmdline_changed()
    local num_str = cmd_line:match('^%d+')
    if num_str then
       peek(winnr, tonumber(num_str))
-      vim.cmd('redraw')
+      cmd('redraw')
    else
       unpeek(winnr, false)
-      vim.cmd('redraw')
+      cmd('redraw')
    end
 end
 
@@ -81,24 +94,24 @@ function numb.on_cmdline_exit()
 end
 
 function numb.setup(user_opts)
-   if user_opts then opts = vim.tbl_extend('force', opts, user_opts) end
-   vim.api.nvim_exec([[
-    augroup numb
-        autocmd!
-        autocmd CmdlineChanged : lua require('numb').on_cmdline_changed()
-        autocmd CmdlineLeave : lua require('numb').on_cmdline_exit()
-    augroup END
-   ]], true)
+   opts = vim.tbl_extend('force', opts, user_opts or {})
+   nvim_exec [[
+      augroup numb
+         autocmd!
+         autocmd CmdlineChanged : lua require('numb').on_cmdline_changed()
+         autocmd CmdlineLeave : lua require('numb').on_cmdline_exit()
+      augroup END
+   ]]
 end
 
 function numb.disable()
    win_states = {}
-   vim.api.nvim_exec([[
+   nvim_exec [[
       augroup numb
-          autocmd!
+         autocmd!
       augroup END
       augroup! numb
-   ]], true)
+   ]]
 end
 
 return numb
